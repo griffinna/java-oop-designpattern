7acb919aecb61d587c729ab9f697ee2b59d4bf3b
Griffin <garam.dev9@gmail.com>
2021-03-26 23:10:33 +0900

    [CHAPTER 02] 일반화 관계와 위임

---
+++ src/java/main/oop/MyStack.java
@@ -0,0 +1,36 @@
+package oop;
+
+import java.util.ArrayList;
+
+public class MyStack {
+
+    public static void main(String[] args) {
+        RioStack<String> stack = new RioStack<>();
+        stack.push("1");
+        stack.push("2");
+//        stack.set(0, "3");
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
+
+class RioStack<String> {
+
+    private ArrayList<String> arList = new ArrayList<>();
+
+    public void push(String element) {
+        arList.add(element);
+    }
+
+    public String pop() {
+        return arList.remove(arList.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return arList.isEmpty();
+    }
+
+    public int size() {
+        return arList.size();
+    }
+}
