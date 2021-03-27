1c301db3bf7a10a091db66def8110d4d0ded2e1e
Griffin <garam.dev9@gmail.com>
2021-03-27 22:15:56 +0900

    [CHAPTER 03] SRP (Single Responsibility Principle: 단일 책임 원칙) 위반 클래스
     - 단 하나의 책임만을 가져야한다.

---
+++ src/java/main/chapter03/SRP/Student.java
@@ -0,0 +1,27 @@
+package chapter03.SRP;
+
+public class Student {
+    public void getCourses() {
+
+    }
+
+    public void addCourses() {
+
+    }
+
+    public void save() {
+
+    }
+
+    public Student load() {
+        return new Student();
+    }
+
+    public void printOnReportCard() {
+
+    }
+
+    public void printOnAttendancdBook() {
+
+    }
+}
