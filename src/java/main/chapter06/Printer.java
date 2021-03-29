f9c57ede203086c43e7f5a904e99c7be5d677385
Griffin <garam.dev9@gmail.com>
2021-03-29 20:41:44 +0900

    [CHAPTER 05] singleton 예제 - solution 1
     - 정적 변수에 인스턴스를 만들어 바로 초기화하는 방법

--- src/java/main/chapter06/Printer.java
+++ src/java/main/chapter06/Printer.java
@@ -2,26 +2,25 @@ package chapter06;
 
 public class Printer {
 
-    private static Printer printer = null;
-    private int counter = 0;
+    // solution 1 : 정적 변수에 인스턴스를 만들어 바로 초기화하는 방법
+    private static Printer printer = new Printer();
 
     private Printer() {}
 
     public static Printer getPrinter() {
-        if (printer == null) {
-            try{
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            printer = new Printer();    // Printer 인스턴스 생성
-        }
+//        if (printer == null) {
+//            try{
+//                Thread.sleep(1);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            printer = new Printer();    // Printer 인스턴스 생성
+//        }
         return printer;
     }
 
     public void print(String resource) {
-        counter++;
-        System.out.println(resource + counter);
+        System.out.println(resource);
     }
 
 }
